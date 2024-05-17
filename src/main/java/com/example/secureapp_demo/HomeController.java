160cb2195aaeb970b494132b14a379ccaa77c2bf
kumar <kumar76199@gmail.com>
2024-05-17 14:48:15 +0530

    changed to restcontroller

--- src/main/java/com/example/secureapp_demo/HomeController.java
+++ src/main/java/com/example/secureapp_demo/HomeController.java
@@ -1,15 +1,16 @@
 package com.example.secureapp_demo;
 
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 
-@Controller
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class HomeController {
 	
-	@GetMapping("/")
+	@GetMapping("/aroha")
 	public String home() {
 		return "home.jsp";
 	}
+
 
 }
